cdfd6d4fb12f765e6090f6b35957c4159659680b
github username <github email address>
2021-01-06 15:40:38 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,39 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toyCollection = document.querySelector('#toy-collection')
+
+  function renderToy(toy) {
+    let card = document.createElement('div')
+    card.classList.add("card")
+    toyCollection.appendChild(card)
+
+    let name = document.createElement('h2')
+    let img = document.createElement('img')
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+
+    name.innerText = toy.name
+    img.src = toy.image
+    img.classList.add("toy-avatar")
+    p.innerText = `liked ${toy.likes} time(s)`
+    btn.innerText = "Like <3"
+    btn.classList.add("like-btn")
+
+    card.appendChild(name)
+    card.appendChild(img)
+    card.appendChild(p)
+    card.appendChild(btn)
+  }
+
+  fetch('http://localhost:3000/toys')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json){
+      for (let toy of json) {
+        renderToy(toy)
+      }
+    });
+
